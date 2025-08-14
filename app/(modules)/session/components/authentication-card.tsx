0c10b601aa177225c402bad7ae68a6a7fdee4fb6
Isaay Sosa Hernández <93836824+iso11ml@users.noreply.github.com>
2025-08-14 10:13:53 -0600

    Add the following public environment variable to the configuration.

--- app/(modules)/session/components/authentication-card.tsx
+++ app/(modules)/session/components/authentication-card.tsx
@@ -10,13 +10,12 @@ interface Props {
 
 const AuthorizationCard = ({ title, description }: Props) => {
   const handleRedirect = () => {
-    const CLIENT_ID = '0416a1fc-d18a-4f93-a8b8-fd5ae59778ae';
-    const TENANT_ID = 'a513d65b-6e93-4b9d-9a53-00f2decda084';
+    const CLIENT_ID = '5244cd55-d3a5-405a-8201-ccffb461e8a4';
+    const TENANT_ID = 'common';
     const REDIRECT_URI = encodeURIComponent('http://localhost:3000/session/authentication');
-    const SCOPES = encodeURIComponent('User.Read offline_access');
-    const STATE = encodeURIComponent('user123-sessiontoken');
+    const SCOPES = encodeURIComponent('https://graph.microsoft.com/User.Read https://graph.microsoft.com/Files.Read offline_access');
 
-    const url = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&response_mode=query&scope=${SCOPES}`;
+    const url = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&response_mode=query&scope=${SCOPES}&prompt=consent`;
     window.location.href = url
   }
 
