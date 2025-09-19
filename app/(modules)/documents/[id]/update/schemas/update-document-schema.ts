2e3ba8c7c0e8cb57dad1fd3101fd81bfdab66229
Martin chacon <chacon0754@outlook.com>
2025-09-18 23:54:52 -0600

    Feature: create update-document-schema on [id] update schema

---
+++ app/(modules)/documents/[id]/update/schemas/update-document-schema.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+import { BASE_DOCUMENT_SCHEMA } from "../../../schema/base-document-form";
+
+const UPDATE_DOCUMENT_SCHEMA = BASE_DOCUMENT_SCHEMA.extend({
+    _id: z.string(),
+});
+
+export { UPDATE_DOCUMENT_SCHEMA };
