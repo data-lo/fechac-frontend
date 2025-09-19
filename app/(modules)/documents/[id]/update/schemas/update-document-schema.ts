c8ef1431bfdf00192f1cfbb5071faef85f1dc28f
Martin chacon <chacon0754@outlook.com>
2025-09-19 02:01:27 -0600

    Update: add metadata to update-document-schema

--- app/(modules)/documents/[id]/update/schemas/update-document-schema.ts
+++ app/(modules)/documents/[id]/update/schemas/update-document-schema.ts
@@ -3,6 +3,7 @@ import { BASE_DOCUMENT_SCHEMA } from "../../../schema/base-document-form";
 
 const UPDATE_DOCUMENT_SCHEMA = BASE_DOCUMENT_SCHEMA.extend({
     _id: z.string(),
+    metadata: z.record(z.any()),
 });
 
 export { UPDATE_DOCUMENT_SCHEMA };
